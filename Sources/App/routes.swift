b53e7642a883b260dd94d886485df723878a0355
Oleg Bragin <>
2024-08-05 13:20:33 +0300

    first step

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,11 +3,11 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.get { req async in
-        "It works!"
+        "It finally works!"
     }
 
     app.get("hello") { req async -> String in
-        "Hello, world!"
+        "Hello, Olga!"
     }
 
     try app.register(collection: TodoController())
