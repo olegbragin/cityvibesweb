abeeb4adddf49a441f1bf892ad1f2503ea2bec81
Oleg Bragin <>
2024-08-05 13:44:15 +0300

    first step

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,7 +3,17 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.get { req async in
-        "It finally works!"
+       html([
+        body([
+            h1(["Type-safe Vapor HTML"]),
+            p([
+                """
+                This is a Vapor plugin that allows you to write type-safe, \
+                transformable, composable HTML views in a Vapor app!
+                """
+            ])
+            ])
+        ])
     }
 
     app.get("hello") { req async -> String in
