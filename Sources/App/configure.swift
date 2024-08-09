d678909e9ae30e7461139247689eca937b99b217
Oleg Bragin <>
2024-08-09 14:48:05 +0300

    Added MVP map to show one route

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,9 +6,6 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
     app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
